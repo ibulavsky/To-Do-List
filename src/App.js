7f052d27d69a8003041814c7aa0f1a8ab05dc94e
ibulavsky <ibulavsky@gmail.com>
2019-10-24 22:50:41 +0300

    Added Material-UI

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
+import Button from "@material-ui/core/Button";
 
 function App() {
     return (
         <div className="App">
             <header className="App-header">
-                Список дел
+                <Button variant="contained" color="primary">
+                    Список дел
+                </Button>
             </header>
         </div>
     );
