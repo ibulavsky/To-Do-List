e5501a8278005c89e343f5fca8276fd1d4d59bc3
ibulavsky <ibulavsky@gmail.com>
2019-10-27 00:49:47 +0300

    Remove MaterialUI. Created Header Component.

--- src/components/AddForm.jsx
+++ src/components/AddForm.jsx
@@ -1,15 +1,13 @@
 import React from "react";
-import Button from "@material-ui/core/Button";
-import Input from "@material-ui/core/Input";
 import styles from "./Styles/AddForm.module.css"
 
 const AddForm = (props) => {
     return (
         <div className={styles.AddForm}>
-            <span> <Input/> </span>
-            <span> <Button variant="contained" color="primary">
-                Добавить
-            </Button> </span>
+            <span> <input/> </span>
+            <span>
+                <button>Добавить</button>
+            </span>
         </div>
     );
 }
