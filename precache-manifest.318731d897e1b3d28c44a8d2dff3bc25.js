45734123fba3b351e99ffee624bf153a225d4cf4
ibulavsky <ibulavsky@gmail.com>
2020-01-08 19:18:49 +0300

    Updates

---
+++ precache-manifest.318731d897e1b3d28c44a8d2dff3bc25.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d230eb1bcd6422b58e9af9d0ec6103b",
+    "url": "/ibulavsky.github.io/wishes-lists/index.html"
+  },
+  {
+    "revision": "a82592fb47a884a7c487",
+    "url": "/ibulavsky.github.io/wishes-lists/static/css/2.fb9ed26a.chunk.css"
+  },
+  {
+    "revision": "e4d70d38295cca57f1c0",
+    "url": "/ibulavsky.github.io/wishes-lists/static/css/main.8ec7eafe.chunk.css"
+  },
+  {
+    "revision": "a82592fb47a884a7c487",
+    "url": "/ibulavsky.github.io/wishes-lists/static/js/2.ddb58e53.chunk.js"
+  },
+  {
+    "revision": "e4d70d38295cca57f1c0",
+    "url": "/ibulavsky.github.io/wishes-lists/static/js/main.6d4edb87.chunk.js"
+  },
+  {
+    "revision": "b1a64c53080c72db8d7e",
+    "url": "/ibulavsky.github.io/wishes-lists/static/js/runtime-main.43ed8b6f.js"
+  }
+]);
